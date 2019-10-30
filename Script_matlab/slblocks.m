37ca7ed078aaf231a2cf386a25bcae9fda716845
Salvatore <salvatore.torsello08@gmail.com>
2019-10-30 13:54:23 +0100

    fine corso 29-10

---
+++ Script_matlab/slblocks.m
@@ -0,0 +1,10 @@
+function blkStruct = slblocks
+        % This function specifies that the library should appear
+        % in the Library Browser
+        % and be cached in the browser repository
+ 
+ 	        % 'My Library' is the library name that appears 
+             % in the Library Browser
+        Browser.Library = 'CorsoSimulinkLib';
+        Browser.Name = 'Corso Simulink Library';
+        blkStruct.Browser = Browser;
